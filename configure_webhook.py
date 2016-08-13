f9bbff9a10c96a3c08fd403185e2caf394598306
Sirjan Kafle <sxk1693@berkeley.edu>
2016-08-13 02:20:37 -0700

    Wrote configure_webhook.py

--- configure_webhook.py
+++ configure_webhook.py
@@ -1,9 +1,35 @@
 from __future__ import print_function
-from twilio.rest.ip_messaging import TwilioIpMessagingClient
+from twilio.rest import TwilioRestClient
+import sys
+import os
 
-account = "AC01c28726d1857eb4ed815b5cf8a7f51b"
-token = "cfb125802b98e767edb6a98015ca1efa"
-client = TwilioIpMessagingClient(account, token)
+APP_ID_FILE_PATH = './app_id.txt'
 
-services = client.services.list()
-print(services)
+
+def configure_webhook(client, url):
+    if os.path.isfile(APP_ID_FILE_PATH):
+        app_id_file = open(APP_ID_FILE_PATH)
+        app_id = app_id_file.read()
+        app = client.applications.get(app_id)
+        if not app:
+            os.remove(APP_ID_FILE_PATH) # App ID is not valid anymore
+            generate_new_application(client, url)
+    else:
+        generate_new_application(client, url)
+
+
+def generate_new_application(client, url):
+    app = client.applications.create(friendly_name='messaging_webhook',
+                                     sms_url=url,
+                                     sms_method="GET")
+    new_file = open(APP_ID_FILE_PATH, 'w')
+    new_file.write(app.sid)
+
+if __name__ == '__main__':
+    account = "AC01c28726d1857eb4ed815b5cf8a7f51b"
+    token = "cfb125802b98e767edb6a98015ca1efa"
+    twilio_client = TwilioRestClient(account, token)
+    ngrok_url = ''
+    for line in sys.stdin:
+        ngrok_url += line
+    configure_webhook(twilio_client, ngrok_url)
