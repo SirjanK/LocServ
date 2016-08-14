742b68bb771be736b9d344f4f2f8d22d87c622cf
Sirjan Kafle <sxk1693@berkeley.edu>
2016-08-13 19:59:45 -0700

    Added shutdown feature in deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/env bash
+trap destroy SIGINT
+function destroy() {
+    kill -9 $(lsof -i:4040 -t)
+    kill -9 $(lsof -i:${port_number} -t)
+    exit 0
+}
 echo "Enter the desired port number above 1024 (eg. 5000):"
 read port_number
 re='^[0-9]+$'
@@ -15,3 +21,14 @@ python3 LocServ.py -p ${port_number} &
 nohup ngrok http ${port_number} &
 sleep 2 #TODO: Update with localhost:4040 being polled for status instead
 curl localhost:4040/api/tunnels/ | python3 ngrok_url.py | python3 configure_webhook.py
+function launch_cancel() {
+    echo "Press c to cancel..."
+    read -n1 -r -p '' key
+    if [ ${key} == "c" ]; then
+        destroy
+    else
+        echo "That's not c -_-"
+        launch_cancel
+    fi
+}
+launch_cancel
--- kill.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-echo "Enter the port number of the server"
-read port_number
-re='^[0-9]+$'
-if ! [[ ${port_number} =~ $re ]] ; then
-    echo "error: Not a number" >&2; exit 1
-fi
-kill -9 $(lsof -i:4040 -t)
-kill -9 $(lsof -i:${port_number} -t)
